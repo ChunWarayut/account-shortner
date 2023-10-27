0d881d33124bccc7a57d87150f93cc5fa9a2d3e1
ChunWarayut <warayut2555@gmail.com>
2023-10-28 05:57:21 +0700

    delete user auth

--- store/auth.ts
+++ store/auth.ts
@@ -41,7 +41,6 @@ export const useAuthStore = defineStore("auth", {
       const token = useCookie("token"); // useCookie new hook in nuxt 3
       this.authenticated = false; // set authenticated  state value to false
       token.value = null; // clear the token cookie
-      this.user = null;
     },
   },
 });
